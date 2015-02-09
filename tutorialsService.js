6ff96679310b1283da16036668694e6de2ad6cca
Brett Heil <bkheil01@louisville.edu>
2015-02-09 03:30:48 -0500

    Update tutorialsService.js

--- tutorialsService.js
+++ tutorialsService.js
@@ -3,7 +3,7 @@ var app = angular.module("TutorialsApp", []);
 app.service("tutorialsService", function($http, $q)
 {
   var deferred = $q.defer();
-  $http.get('github.com/bkheil01/Assignment6/blob/gh-pages/tutorials.json').then(function (data)
+  $http.get("tutorials.json").then(function (data)
   {
     deferred.resolve(data);
   });
